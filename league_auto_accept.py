02048f38457e29f83bcc502ddd0033c058baea61
Raid7 <thmrgb8@gmail.com>
2021-05-23 01:04:34 -0900

    fixed (hopefully)

--- league_auto_accept.py
+++ league_auto_accept.py
@@ -1,5 +1,11 @@
 import pyautogui
 from time import sleep
+global accepted
+global banned
+global chose
+accepted = False
+banned = False
+chose = False
 
 def findimage(image):
     cords = pyautogui.locateOnScreen(image)
@@ -14,20 +20,24 @@ def click(img):
 def main():
     to_ban = input("what champion to ban?: ")
     to_select= input("what champion to play?: ")
-    global brek
+    global accepted,banned,chose
+    print(accepted,banned,chose)
     print("[+] Started Searching [+]")
     #keep searching for the accept button
-    while findimage("accepted.png") is None:
+    while findimage("accepted.png") is None and accepted == False:
         sleep(0.1)
         try:
             click("accept.png")
         except:
             print("cant find accept")
+    accepted = True
     print("Game Started!")
-    while findimage("ban_search.png") is None:
+    while findimage("ban_search.png") is None and banned == False:
         sleep(0.1)
+    print("[+] Found Ban Button [+]")
     ban_cords = findimage("ban_search.png")
     click("ban_search.png")
+    print("[+] Clicked Ban Button [+]")
     sleep(0.3)
     pyautogui.write(to_ban,interval=0.25)
     sleep(0.5)
@@ -36,8 +46,9 @@ def main():
     click("ban_button.png")
     sleep(0.5)
     print("Banned "+ to_ban + "!")
+    banned = True
     print("Waiting for champ select option")
-    while findimage("normal_search.png") is None:
+    while findimage("normal_search.png") is None and chose == False:
         sleep(0.5)
     normal_search_cords = findimage("normal_search.png")
     print("Selecting!")
@@ -49,5 +60,6 @@ def main():
     sleep(1)
     click("lock_button.png")
     print("Selected [{0}] And Banned [{1}]".format(to_select,to_ban))
+    chose = True
 if __name__ == "__main__":
     main()
